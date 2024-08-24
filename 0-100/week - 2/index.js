0d95d8a7c80af4947869428fa0bd1ca16a7f3c6c
“konhito” <konhito02@gmail.com>
2024-08-24 15:15:09 +0530

    promises

--- 0-100/week - 2/index.js
+++ 0-100/week - 2/index.js
@@ -23,3 +23,27 @@
 //   let square2 = fn(b);
 //   return square1 + square2;
 // }
+//
+// sync - means things are happening one after another. sequentials.
+// async - wait for the task to compelet and(not until we add await) hold the thread there.
+// 1.read a file 2.fetch 3.setTimeout are a async function
+// simple async example
+
+// function onDone() {
+//   console.log("hii there!!"); <- this thing waited 2 sec to be excuted
+// }
+// setTimeout(onDone, 2000);
+// console.log("after 2 sec"); <- this thing excuted immediatly
+
+// promises -> more readable to write callbacks
+// also a class js provides you out of the box.
+// function myOwnSetTimeout(duration) {
+//   let p = new Promise(function (resolve) { <- it expects a function and first paramerter in the function is the resolve
+//     setTimeout(resolve, 2000);
+//   });
+//   return p;
+// }
+
+// myOwnSetTimeout(1000).then(() => {
+//   console.log("log it first ");
+// });
