4fa73b8738fcaa2e2e1a884f628bb14d6f3da8b3
Aditya Singh <132154862+konhito@users.noreply.github.com>
2024-08-25 11:05:29 +0530

    Create diff input in express http sever.js

---
+++ 0-100/week - 2/diff input in express http sever.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const port = 3000;
+const app = express();
+
+app.use(express.json());
+
+//--------------------------------------------query ----------------------------------------
+// app.get("/", function (req, res) {
+//   const msg = req.query;
+//   console.log(msg);
+//   res.json(req.body);
+// });
+// output -> a json with all the query (parameter and value) ex -> { msg: 'shheeee', hii: 'bye' }
+// app.get("/", function (req, res) {
+//   const msg = req.query.msg; // -> we are only talking the msg parameter and its value not taking all the parameters
+//   console.log(typeof msg); ///-> string
+//   res.json(msg); // -> ex - > "shhee" a parameter = msg and value = "sheee"
+// });
+//--------------------------------------------------------------------------------------------------------
+
+//----------------------------Body---------------------------------------------------
+//for body we need a middleware -> app.use(express.json()) make all the incoming body to json formate as the body also send the formate to json format
+app.get("/", function (req, res) {
+  const msg = req.body; //requesting the whole body
+  console.log(typeof msg); ///-> OBJEXT
+  res.json(msg); // -> ex - > {"hii" : "there"}
+});
+//--------------------------------------------------------------------------------------------------------------------------
+
+app.listen(port, () => {
+  console.log(`Server started in the ${port}`);
+});
