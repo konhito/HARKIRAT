8ce935daaf9c36896df405caf25fa142575cac90
konhtio <konhito02@gmail.com>
2024-08-26 22:18:48 +0530

    middleware with functon

--- 0-100/week3/middleware.js
+++ 0-100/week3/middleware.js
@@ -2,24 +2,56 @@
 
 // End the request-response cycle. // Call the next middleware function in the stack. // If the current middleware function does not end the request-response cycle, it must call next() to pass control to the next middleware function. Otherwise, the request will be left hanging.
 // An Express application can use the following types of middleware:
+
+// const express = require("express");
+// const app = express();
+// const port = 3000;
+// app.use(express.json());
+// app.get("/", (req, res) => {
+//   const kidneyId = req.query.ID;
+//   const username = req.headers.username;
+//   const pass = req.headers.pass;
+//   console.log(username); // this is authentication
+//   if (username != "aditya" && pass != "pass") {
+//     res.status(403).json({ msg: "User does not exist" });
+//     return;
+//   } //-- this is input validation
+//   if (kidneyId != 1 && kidneyId != 2) {
+//     res.send(411).json({ msg: "wrong inputs" });
+//     return;
+//   }
+// });
+// app.listen(port, () => {
+//   console.log(`server running in port 3000`);
+// });
+
+// - insted of repeating code we can do this make a function and check for all the validations.
 const express = require("express");
 const app = express();
 const port = 3000;
-app.use(express.json());
+
+app.use(express.json()); // To parse JSON request bodies
+
+function userNameChecker(username, pass) {
+  return username === "aditya" && pass === "pass";
+}
+
 app.get("/", (req, res) => {
-  const kidneyId = req.query.ID;
-  const username = req.headers.username;
-  const pass = req.headers.pass;
-  console.log(username); // this is authentication
-  if (username != "aditya" && pass != "pass") {
-    res.status(403).json({ msg: "User does not exist" });
-    return;
-  } //-- this is input validation
-  if (kidneyId != 1 && kidneyId != 2) {
-    res.send(411).json({ msg: "wrong inputs" });
+  const { username, pass } = req.headers;
+
+  if (!username || !pass) {
+    res.status(400).json({ msg: "Username and password are required" });
     return;
   }
+
+  if (!userNameChecker(username, pass)) {
+    res.status(403).json({ msg: "Invalid user" });
+    return;
+  }
+
+  res.status(200).json({ msg: "Welcome, authenticated user!" });
 });
+
 app.listen(port, () => {
-  console.log(`server running in port 3000`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
