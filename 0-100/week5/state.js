174cf19dd623c08226a1a5c6b01320cb9f64c50b
konhtio <konhito02@gmail.com>
2024-09-01 11:04:16 +0530

    state

---
+++ 0-100/week5/state.js
@@ -0,0 +1,5 @@
+//state , components and re rendering
+// state is like a class we make a state and put diffrence components(like a object) in the classs
+// updating the state is the re-renderting
+
+//you usually have to define    all your componenets once and then all the you have to do is update the state of the componests,Reacts take care of the compoonets.
