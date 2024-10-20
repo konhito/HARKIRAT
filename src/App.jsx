a0b3401bebe078ff6408d17233aae14633458cff
konhito <konhito02@gmail.com>
2024-10-20 13:50:41 +0530

    basic react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/Header";
+import Form from "./components/Form";
+
+function App() {
+  return (
+    <>
+      <Header title="hello world"></Header>
+      <Form></Form>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Form.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+
+const Form = () => {
+  const [name, setName] = useState("");
+
+  const [data, setData] = useState([
+    {
+      name: "Hello world",
+    },
+  ]);
+
+  function pushInArray(e) {
+    let obj = { name: e };
+    setData((prevData) => [...prevData, obj]);
+  }
+
+  function inputNameChange(e) {
+    setName(e.target.value);
+  }
+
+  function changeName() {
+    pushInArray(name);
+  }
+
+  function deleteName(indexToDelete) {
+    setData((prevData) =>
+      prevData.filter((_, index) => index !== indexToDelete)
+    );
+  }
+
+  function displayNames() {
+    return data.map((item, index) => (
+      <div key={index}>
+        {item.name}{" "}
+        <input type="checkbox" onChange={() => deleteName(index)}></input>{" "}
+      </div>
+    ));
+  }
+
+  return (
+    <div>
+      <input
+        placeholder="Enter your name"
+        value={name}
+        onChange={inputNameChange}
+      />
+
+      <button onClick={changeName}>Submit</button>
+      <h2>Submitted Names:</h2>
+      {displayNames()}
+    </div>
+  );
+};
+
+export default Form;
